4b6183c73335785291020790427ef7fe79d52612
Fabien Dubosson <fabien.dubosson@gmail.com>
2016-01-29 17:33:00 +0100

    Improved

--- keyboard/ergodox_ez/keymaps/StreakyCobra/keymap.c
+++ keyboard/ergodox_ez/keymaps/StreakyCobra/keymap.c
@@ -22,44 +22,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |   `    |   1  |   2  |   3  |   4  |   5  | Num  |           |Play/P|   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |  Tab   |   Q  |   W  |   E  |   R  |   T  | Symb |           |      |   Y  |   U  |   I  |   O  |   P  |   ]    |
+ * |  Tab   |   Q  |   W  |   E  |   R  |   T  | Symb |           | VolUp|   Y  |   U  |   I  |   O  |   P  |   ]    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |TempSymb|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
- * |--------+------+------+------+------+------|Media |           |      |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|Media |           | VolDn|------+------+------+------+------+--------|
  * | LShift |[/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |/ ctrl|\ shift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  Z   |      |      |      |AltGr |                                       | Alt  |      |      |      |   =   |
+ *   |      |  Z   |      |  Alt | LGui |                                       | AltGr| Alt  |      |      |   =   |
  *   `----------------------------------'                                       `-----------------------------------'
  *                                       ,--------------.       ,-------------.
- *                                       |Esc/Alt| NumL |       | Home | End  |
+ *                                       |  Esc  | NumL |       | Home | End  |
  *                                ,------+-------+------|       |------+------+------.
- *                                |      |       |      |       | PgUp |      |      |
- *                                |BackSp|  LGui |------|       |------|Enter |Space |
- *                                |ace   |       | Del  |       | PgDn |      |      |
+ *                                |      |       |      |       |      |      |      |
+ *                                |BackSp|  Del  |------|       |------|Enter |Space |
+ *                                |ace   |       |  `   |       |RAL(S)|      |      |
  *                                `---------------------'       `--------------------'
  */
 [BASE] = KEYMAP(
 
-        KC_GRV,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   TG(NUMR),
+        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, TG(NUMR),
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         MO(SYMB),       KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT, CTL_T(KC_LBRC),      KC_X,   KC_C,   KC_V,   KC_B,   TG(MDIA),
-        KC_Z,           KC_TRNS,     KC_TRNS, KC_TRNS, KC_RALT,
+        KC_TRNS,        KC_Z,      KC_TRNS, KC_LALT, KC_LGUI,
 
                                                       KC_ESC,  KC_NLCK,
                                                                KC_TRNS,
-                                             KC_BSPC,KC_LGUI,  KC_DELT,
+                                             KC_BSPC,KC_DELT,  KC_GRV,
 
         // right hand
              KC_MPLY,     KC_6,   KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,
-             KC_TRNS,     KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     KC_RBRC,
+             KC_VOLU,     KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     KC_RBRC,
                           KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
-             KC_TRNS,     KC_N,   KC_M,    KC_COMM, KC_DOT,  CTL_T(KC_SLSH),  SFT_T(KC_BSLS),
-                             KC_LALT, KC_TRNS, KC_TRNS, KC_TRNS, KC_EQL,
+             KC_VOLD,     KC_N,   KC_M,    KC_COMM, KC_DOT,  CTL_T(KC_SLSH),  SFT_T(KC_BSLS),
+                             KC_RALT, KC_LALT, KC_TRNS, KC_TRNS, KC_EQL,
 
              KC_HOME, KC_END,
-             KC_PGUP,
-             KC_PGDN, KC_ENT,  KC_SPC
+             KC_TRNS,
+        RALT(KC_SPC), KC_ENT,  KC_SPC
     ),
 /* Numeric Layer
  *
@@ -109,11 +109,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      | Home | PgUp | ScrUp|  End |      |   F12  |
+ * |        |      |      |      | CTL_Y|      |      |           |      |      | Home | PgUp | WH_U | CTL_U|   F12  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |      |      |      | Esc  |      |------|           |------| Left | Down |  Up  | Right|      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      | PgDn | ScrDn|      |      |        |
+ * |        |      |      |      | CTL_E|      |      |           |      |      |  End | PgDn | WH_D | CTL_D|        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
@@ -121,29 +121,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       |      |      |      |
- *                                 |      |      |------|       |------|      |      |
- *                                 |      |      |      |       |      |      |      |
+ *                                 | SPACE| Enter|------|       |------| COPY | PASTE|
+ *                                 |      |      |      |       | CUT  |      |      |
  *                                 `--------------------'       `--------------------'
  */
 [SYMB] = KEYMAP(
        // left hand
        KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,LCTL(KC_X),KC_TRNS,KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_ESC,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,LCTL(KC_F),KC_TRNS,KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
                                        KC_TRNS,KC_TRNS,
                                                KC_TRNS,
-                               KC_TRNS,KC_TRNS,KC_TRNS,
+                               KC_SPC,KC_ENT,KC_TRNS,
        // right hand
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_HOME, KC_PGUP, KC_BTN4,  KC_END,    KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_HOME, KC_PGUP, KC_WH_U,    LCTL(KC_S), KC_TRNS,
                 KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,    KC_PLUS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_PGDN, KC_BTN5, KC_TRNS,    KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS,  KC_END, KC_PGDN, KC_WH_D,    LCTL(KC_I), KC_TRNS,
                          KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS
+       M(MCUT), M(MCOPY), M(MPSTE)
 ),
 /* Media and mouse keys
  *
